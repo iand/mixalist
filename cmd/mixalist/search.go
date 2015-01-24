3d559ef112b9399750f7df33f8ca5953ec26217d
Ian Davis <nospam@iandavis.com>
2015-01-24 16:03:44 +0000

    search youtube

--- cmd/mixalist/search.go
+++ cmd/mixalist/search.go
@@ -4,9 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/iand/mixalist/pkg/playlist"
 	"github.com/iand/mixalist/pkg/search"
-	"github.com/iand/youtube"
 )
 
 type SearchResponse struct {
@@ -22,26 +20,8 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(200)
-
-	client := youtube.New()
-	feed, err := client.VideoSearch(query)
-	if err != nil {
-		writeJSON(w, resp)
-		return
-	}
-
-	entries := make([]playlist.Entry, len(feed.Entries))
-	for i, e := range feed.Entries {
-		entries[i] = playlist.Entry{
-			Title: e.Title.String(),
-			Ytid:  e.ID.String(),
-		}
-	}
-
-	_ = entries
-	writeJSON(w, feed.Entries)
+	resp.Results = search.Search(query)
+	writeJSON(w, resp)
 }
 
 func writeJSON(w http.ResponseWriter, v interface{}) {
