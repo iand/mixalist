9e24e42cbbebfe5e0434975293a0135176eb910e
Kier Davis <kierdavis@gmail.com>
2015-01-24 12:38:25 +0000

    Revise database migration steps again

--- pkg/db/updates.go
+++ pkg/db/updates.go
@@ -2,9 +2,9 @@ package db
 
 // How to perform a database update:
 // 1) increment the latest version number (here and in schema.sql)
-// 2) add a DatabaseUpdate for migration of existing data to Updates
-// 3) if the change required modificiation of the table schemas, update the
+// 2) if the change requires modificiation of the table schemas, update the
 //    schemas here and in schema.sql
+// 3) add a DatabaseUpdate for migration of existing data to Updates
 // 4) commit all of the above as a single commit
 
 // Latest version of the database
