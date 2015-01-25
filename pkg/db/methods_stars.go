dc8296000c5a42ac34d650c5c63429ca484fa26c
Freya Davis <freya@oldstick.com>
2015-01-25 17:29:07 +0000

    added toggle star database method

--- pkg/db/methods_stars.go
+++ pkg/db/methods_stars.go
@@ -19,15 +19,37 @@ func (db *Database) GetPlaylistStars(pid playlist.PlaylistID) (stars int, err er
 }
 
 func (db *Database) ToggleStar(uid playlist.UserID, pid playlist.PlaylistID) (bool, error) {
+	if db.tx.tx == nil {
+		return false, wrapError(1, NotInTransactionError)
+	}
 
-	// row := db.getQueryable().QueryRow("select count(*) from mix_playlist_star where pid = $1", pid)
-	// err = row.Scan(&stars)
-	// if err != nil {
-	// 	if isNoRowsError(err) {
-	// 		err = InvalidPlaylistError
-	// 	}
-	// 	return 0, err
-	// }
+	var count int
+
+	row := db.getQueryable().QueryRow("select count(*) from mix_playlist_star where pid = $1 and uid = $2", pid, uid)
+	err := row.Scan(&count)
+	if err != nil {
+		// something bad happpened
+		return false, err
+	}
+
+	if count == 0 {
+		// No star, so add one
+		_, err = db.tx.Exec("insert into mix_playlist_star values ($1, $2)", pid, uid)
+		if err != nil {
+			db.RollbackTx()
+			return false, err
+		}
+		return true, nil
+
+	} else {
+		// We have star, remove it
+		_, err = db.tx.Exec("delete from mix_playlist_star where pid = $1 and uid = $2", pid, uid)
+		if err != nil {
+			db.RollbackTx()
+			return false, err
+		}
+		return false, nil
+	}
 
 	return false, nil
 
